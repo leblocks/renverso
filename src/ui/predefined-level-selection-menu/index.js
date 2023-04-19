76c3773d36eab9fc575b72ef41e737af6fe18c5d
alexander.g <alexander.g@qualisystems.com>
2023-04-19 16:54:31 +0300

    feat: level selector is now scrollable

--- src/ui/predefined-level-selection-menu/index.js
+++ src/ui/predefined-level-selection-menu/index.js
@@ -3,7 +3,19 @@ import {
     LOCATION_LEVEL_SELECTION_MENU,
 } from '../../state/consts.js';
 
+import { ELEMENT_MENU_ITEM } from '../../theme/consts.js';
+
 import { goto } from '../../utils/index.js';
+
+import {
+    applyStyles,
+    getStylesFor,
+} from '../../theme/index.js';
+
+import {
+    getClassList,
+    createElement,
+} from '../../web-api/index.js';
 
 import {
     initPredefinedLevel,
@@ -16,24 +28,44 @@ import {
     menuTitle,
 } from '../components/index.js';
 
-import { getState } from '../../state/index.js';
+import {
+    getState,
+    addStateObserver,
+} from '../../state/index.js';
 
 const startGame = (level) => {
     initPredefinedLevel(level);
     goto(LOCATION_GAME);
 };
 
-export default function levelSelectionMenu() {
+const levelSelector = () => {
+    const element = createElement('div');
+    getClassList(element).add('ui-level-selector-container');
+
+    // set initial styling on element
+    applyStyles(element, getStylesFor(ELEMENT_MENU_ITEM, getState().theme));
+    addStateObserver(['theme'], ({ theme }) => {
+        applyStyles(element, getStylesFor(ELEMENT_MENU_ITEM, theme));
+    }, 'menu-item-theme-observer');
+
     const { finishedLevelIds } = getState();
-    const levels = getPredefinedLevels()
+    getPredefinedLevels()
         .map((level, i) => {
             const finished = finishedLevelIds.includes(level.id) ? ' (done)' : '';
-            return menuItem(`Level ${i + 1}${finished}`, () => startGame(level));
-        });
+            const btn = menuItem(`Level ${i + 1}${finished}`, () => startGame(level));
+            getClassList(btn).remove('ui-menu-item');
+            getClassList(btn).add('ui-level-selector-button');
+            return btn;
+        })
+        .forEach((level) => element.appendChild(level));
 
+    return element;
+};
+
+export default function levelSelectionMenu() {
     return menu([
         menuTitle('Levels'),
-        ...levels,
+        levelSelector(),
         menuItem('Back', () => goto(LOCATION_LEVEL_SELECTION_MENU)),
     ]);
 }
